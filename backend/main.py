d96dfd6a251b467adf04fa1cdded03565b198d0c
agnieszka <agnieszka.sobala@yandex.com>
2026-01-18 18:52:12 +0000

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # Local development
         "https://*.vercel.app",  # All Vercel deployments
+        "https://escape-rooms-project.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
